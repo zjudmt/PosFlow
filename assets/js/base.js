aa2552fbd263924587abc8fff1c2646de5d00c6e
syby119 <syby119@126.com>
2019-01-30 05:14:17 +0800

    add keyboard event

--- assets/js/base.js
+++ assets/js/base.js
@@ -195,11 +195,11 @@ function init(argument) {
 		h: 800,
 		ratio: 24/5,
 		fps: 25,
-		src: "/resources/PosFlow/2min.mp4",
+		src: "/resources/PosFlow/first_half.mp4",
 	};
 	source_data = {
 		fps: 25,
-		src: "/resources/PosFlow/tracklets.json",
+		src: "/resources/PosFlow/Germany_tracklets.json",
 	}
 	
 	past_duration = 5 * source_video.fps;
@@ -212,11 +212,11 @@ function init(argument) {
 	selected = [];
 	initVideo();
 	initSVG();
+	initKeyBoardEvent();
 
 	d3.json(source_data.src, function(error, data){
 		data = filterData(data);
 		tracklets = initData(data);
-		// tracklets = filterData(tracklets);
 		cur_data = getTrackletsByFrame(tracklets, 0)
 		current_tracklets = cur_data[0];
 		path_tracklets = cur_data[1];
@@ -325,6 +325,105 @@ function updateLayout() {
 		.style("top", layout.new_video.y + "px" )
 }
 
+function initKeyBoardEvent(){
+    document.onkeydown = function(event){
+    	// 多浏览器支持
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+        // 按 <- 向前播放
+        if(e && !event.ctrlKey && e.keyCode==37){
+            console.log("<-");
+            var time = video.property("currentTime");
+            if(video.property("paused")){
+            	video.property("currentTime", time-1/source_video.fps);
+            }
+            else{
+            	video.property("currentTime", time-125/source_video.fps);
+            }
+        }
+        // 按 -> 向后播放
+        else if(e && !event.ctrlKey && e.keyCode==39){
+            console.log("->");
+            var time = video.property("currentTime");
+            if(video.property("paused")){
+            	video.property("currentTime", time+1/source_video.fps);
+            }
+            else{
+            	video.property("currentTime", time+125/source_video.fps);
+            }
+        }
+        // 按 Up 向前跳到一个tracklet开头
+        else if(e && e.keyCode==38){
+            console.log("Up: to be discussed");
+        }
+        // 按 Down 向后跳到一个tracklet结尾
+        else if(e && e.keyCode==40){
+            console.log("Down: to be discussed");
+        }          
+        // 按 空格 暂停/开始
+        else if(e && e.keyCode==32){
+            console.log("Space");
+            if(video.property("paused")){
+            	video._groups[0][0].play();
+            }
+            else{
+            	video._groups[0][0].pause();
+            }
+        }
+        // 按 Ctrl + <- 减速播放
+        else if(e && event.ctrlKey && e.keyCode==37){
+            console.log("Ctrl + <-");
+            var speed = video.property("playbackRate");
+            if(speed > 0.125)
+            	video.property("playbackRate", speed/2);
+        }
+        // 按 Ctrl + -> 加速播放
+        else if(e && event.ctrlKey && e.keyCode==39){
+            console.log("Ctrl + ->");
+            var speed = video.property("playbackRate");
+            if(speed < 8)
+            	video.property("playbackRate", speed*2);
+        }
+        // 按 Ctrl + S 保存
+        else if(e && event.ctrlKey && e.keyCode==83){
+            console.log("Ctrl + S: ask lin to do");
 
+        }
+        // 按 Ctrl + O 载入
+        else if(e && event.ctrlKey && e.keyCode==79){
+            console.log("Ctrl + O: ask lin to do");
+
+        }
+        // 按 Ctrl + C 合并
+        else if(e && event.ctrlKey && e.keyCode==67){
+            console.log("Ctrl + C: ask lin to do");
+
+        }
+        // 按 Ctrl + X 剪断
+        else if(e && event.ctrlKey && e.keyCode==79){
+            console.log("Ctrl + X: ask lin to do");
+
+        }
+        // 按 Ctrl + D 删除
+        else if(e && event.ctrlKey && e.keyCode==68){
+            console.log("Ctrl + D: ask lin to do");
+
+        }
+        // 按 Ctrl + Z 撤销
+        else if(e && event.ctrlKey && e.keyCode==90){
+            console.log("Ctrl + Z: to be finished");
+
+        }
+        // 按 Ctrl + + 放大
+        else if(e && event.ctrlKey && e.keyCode==99){
+            console.log("Ctrl + +: to be finished");
+
+        }
+        // 按 Ctrl + - 缩小
+        else if(e && event.ctrlKey && e.keyCode==101){
+            console.log("Ctrl + -: to be finished");
+
+        }
+	};
+}
 
 addLoadEvent(init);
